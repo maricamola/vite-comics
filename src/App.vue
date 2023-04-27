34f791e83c4970dc6c5d661546087003291c73ad
maricamola <maricamola98@libero.it>
2023-04-27 15:49:14 +0200

    basic structure created

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>HEADER</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.container{
+  h1{
+    background-color:cadetblue;
+  }
+}
+</style>
