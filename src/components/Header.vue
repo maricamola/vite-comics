2ad4f1b72283eb09ef68831a3174b5e43bbe7b91
maricamola <maricamola98@libero.it>
2023-04-27 18:17:57 +0200

    header added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,10 +5,49 @@ export default {
 </script>
 
 <template>
-  <div class="logo">
+  <header>
+    <div class="logo">
     <img src="/dc-logo.png" alt="Logo dc">
   </div>
+
+  <nav>
+    <ul>
+      <li><a href="#">Characters</a></li>
+      <li><a href="#">comics</a></li>
+      <li><a href="#">movies</a></li>
+      <li><a href="#">tv</a></li>
+      <li><a href="#">games</a></li>
+      <li><a href="#">collectibles</a></li>
+      <li><a href="#">videos</a></li>
+      <li><a href="#">fans</a></li>
+      <li><a href="#">news</a></li>
+      <li><a href="#">shop</a></li>
+    </ul>
+  </nav>
+  </header>
+  
 </template>
-<style>
+<style lang="scss" scoped>
+@use '../scss/general/variables' as *;
+@use '../scss/general/mixin' as *;
+
+
+header{
+  @include centerFlex('horizontal');
+  color: black;
+  padding: 50px;
+  align-items: center;
+  justify-content: space-between;
+  ul{
+  @include centerFlex('horizontal');
+  margin-bottom: 2rem;
+  a{
+    display: inline-block;
+    text-transform: uppercase;
+    margin: 1rem;
+    color: black;
+  }
+}
+}
 
 </style>
